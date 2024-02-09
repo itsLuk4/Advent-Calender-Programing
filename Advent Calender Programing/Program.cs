8970e780ad37eb58bc746ed778325b40edf5fe60
Luka Živko <luka.zivko9@gmail.com>
2024-02-09 14:45:18 +0100

    Add project files.

---
+++ Advent Calender Programing/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Calender_Programing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int startingFloor = 0;
+            int positiveValue = 0;
+            int negativeValue = 0;
+            Console.WriteLine("Write a random number of paranthesis where " +
+                "'(' equals 1 and ')' equals -1 to bring the end result!\n" +
+                "Enter here:");
+            string paranthesis = Console.ReadLine();
+            foreach (char s in paranthesis)
+            {
+                if (s == '(')
+                {
+                    positiveValue += 1;
+                }
+                else if (s == ')')
+                {
+                    negativeValue += -1;
+                }
+
+                startingFloor = positiveValue + negativeValue;
+            }
+            Console.WriteLine(startingFloor);
+        }
+    }
+}
