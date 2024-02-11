ea587b9a370178d1d0f256543f05fef5b12addf1
Luka Živko <luka.zivko9@gmail.com>
2024-02-11 18:21:21 +0100

    Changed name.

--- Advent Calender Programing/Properties/AssemblyInfo.cs
+++ Advent Calender Programing/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Advent Calender Programing")]
+[assembly: AssemblyTitle("SantaFloorSearch")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Advent Calender Programing")]
+[assembly: AssemblyProduct("SantaFloorSearch")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
